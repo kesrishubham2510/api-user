d2ed8f8199b9c6b0e7e3161f5d283543d43b8fd3
kesrishubham2510 <shubhamkeshari90@gmail.com>
2025-11-02 16:38:19 +0530

    * Synced with DB columns

--- src/main/java/com/myreflectionthoughts/user/datamodel/entity/User.java
+++ src/main/java/com/myreflectionthoughts/user/datamodel/entity/User.java
@@ -10,16 +10,16 @@ import org.hibernate.annotations.UuidGenerator;
 public class User{
 
     @Id
-    @Column(name="userid")
+    @Column(name="user_id")
     private String userId;
 
-    @Column(name = "firstname")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "lastname")
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name="username")
+    @Column(name="user_name")
     private String username;
 
     @Column(name="email")
